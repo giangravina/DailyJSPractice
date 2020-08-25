efbdc93457d3c975ae637a50d992d00a62515ec7
Gian <giangravina@gmail.com>
2020-08-25 11:45:49 -0400

    part 2 coding challenge

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
+/*****************************
+* CODING CHALLENGE 2
+*/
+
+/*
+
+John and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
+John likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+Implement a tip calculator using objects and loops:
+1. Create an object with an array for the bill values
+2. Add a method to calculate the tip
+3. This method should include a loop to iterate over all the paid bills and do the tip calculations
+4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+
+
+*/
+
+var john = {
+    fullName: 'Johnny Woodpecker',
+    bills: [124, 48, 268, 42],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+
+        for (var i = 0; i < this.bills.length; i++) 
+        {
+          //determine percentage based on tipping rules
+          var percentage;
+          var bill = this.bills[i];
+
+          if (bill < 50)
+          {
+            percentage = .2;
+          } else if (bill > 50 && bill < 200 )
+          {
+            percentage = .15;   
+          } else {
+            percentage = .10;
+          }
+
+
+          //add result to corresponding arrays
+          this.tips[i] = bill * percentage;
+          this.finalValues[i] = bill + bill * percentage;
+        }
+    }
+
+}
+
+john.calcTips();
+console.log(john);
+
+
+
+
+
+
+
 /* Coding Challenge 1 */
 /* 
 Mike and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
